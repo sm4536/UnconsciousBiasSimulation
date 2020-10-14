c4a1c64aebcd3758747a937e796e4f50e39bfe08
Shivani Aggarwal <sa3664@drexel.edu>
2020-10-13 21:53:27 -0400

    Header Component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Nav from "react-bootstrap/Nav";
+import "../../index.css";
 
 class Header extends Component {
   render() {
@@ -8,8 +9,8 @@ class Header extends Component {
         <Nav defaultActiveKey="/home" className="justify-content-end header">
           <h4>Unconscious Bias Simulation</h4>
           <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="">Create Profile</Nav.Link>
-          <Nav.Link href="">View Profile</Nav.Link>
+          <Nav.Link href="/createProfile">Create Profile</Nav.Link>
+          <Nav.Link href="/viewProfile">View Profile</Nav.Link>
         </Nav>
         <br />
         <br />
--- frontend/src/components/Header/Header.spec.js
+++ frontend/src/components/Header/Header.spec.js
@@ -6,12 +6,12 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("Header Component", () => {
-  test("renders", () => {
+describe("Header", () => {
+  it("Header should exist", () => {
     const links = [
       { link: "http://localhost:3000/home", text: "Home" },
-      { link: "", text: "Create Profile" },
-      { link: "", text: "View Profile" },
+      { link: "http://localhost:3000/createProfile", text: "Create Profile" },
+      { link: "http://localhost:3000/viewProfile", text: "View Profile" },
     ];
     const wrapper = shallow(<Header links={links} />);
     expect(wrapper.exists()).toBe(true);
